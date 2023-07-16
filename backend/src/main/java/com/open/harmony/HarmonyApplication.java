f436012fbf175b9edf1eecd69b24dede9f843a17
JiaZiXin <1484485927@qq.com>
2023-07-16 21:12:31 +0800

    基础框架

---
+++ backend/src/main/java/com/open/harmony/HarmonyApplication.java
@@ -0,0 +1,17 @@
+package com.open.harmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author jzx
+ */
+@SpringBootApplication
+public class HarmonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonyApplication.class, args);
+    }
+
+}
