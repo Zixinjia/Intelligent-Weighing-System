47425b3ff6e02de641a1063d272b574c5018ab42
jzx <1484485927@qq.com>
2024-10-09 11:46:11 +0800

    前端代码

---
+++ frontend/entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,63 @@
+import UIAbility from '@ohos.app.ability.UIAbility';
+import hilog from '@ohos.hilog';
+import window from '@ohos.window';
+
+export default class EntryAbility extends UIAbility {
+  onCreate(want, launchParam) {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+  }
+
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+    windowStage.loadContent('pages/Index', (err, data) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return;
+      }
+      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+    });
+    this.setLandscape();
+    this.setSystemBar()
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+  }
+
+  //将应用设置为横屏
+  private async setLandscape() {
+    //将应用设置为横屏
+    let promise = window.getLastWindow(this.context);
+    promise.then((data) => {
+      //切换成横屏
+      let orientation = window.Orientation.LANDSCAPE;
+      data.setPreferredOrientation(orientation, (err) => {
+      });
+    })
+  }
+
+
+  // 隐藏应用导航栏
+  private async setSystemBar() {
+    let windowClass = await window.getLastWindow(this.context)
+    //设置导航栏，状态栏不可见
+    await windowClass.setWindowSystemBarEnable([])
+  }
+}
